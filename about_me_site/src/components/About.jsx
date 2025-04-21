e9ea71ae46cef0556b873d5258bf230b9cca4a1c
Rikuto <mikadorikuto@gmail.com>
2025-04-21 23:42:30 +0900

    Remove about_me_site

--- about_me_site/src/components/About.jsx
+++
@@ -1,9 +0,0 @@
-export default function About() {
-    return (
-      <section className="pt-[50px] pb-20 bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
-        <h2>About Me</h2>
-        <p>ここに自己紹介を書く予定。</p>
-      </section>
-    )
-  }
-  
--- about_me_site/src/components/Blog.jsx
+++
@@ -1,9 +0,0 @@
-export default function Blog() {
-    return (
-      <section>
-        <h2 className="text-3xl font-bold mb-4">Blog</h2>
-        <p>ここにブログ記事一覧を表示する予定。</p>
-      </section>
-    );
-  }
-  
