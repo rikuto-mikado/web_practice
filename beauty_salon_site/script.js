564d1495c9d2b3fb068ad8fee63eaa97abd2f005
Rikuto <mikadorikuto@gmail.com>
2025-05-10 13:42:58 +0900

    updated my beauty_salon_site

--- beauty_salon_site/script.js
+++ beauty_salon_site/script.js
@@ -14,3 +14,32 @@ for (let i = 0; i < items.length; i++) {
     };
     items[i].animate(keyframes, options);
 }
+
+
+const items02 = document.querySelectorAll('.animated-kirin');
+
+const showKirin = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const i = Array.from(items02).indexOf(entry.target);
+
+      entry.target.animate({
+        opacity: [0, 1],
+        translate: ['0 50px', '0 0']
+      }, {
+        duration: 2000,
+        delay: i * 300,
+        fill: 'forwards',
+        easing: 'ease-out'
+      });
+      
+      observer.unobserve(entry.target);
+    }
+  });
+};
+
+const kirinObserver = new IntersectionObserver(showKirin);
+
+for (let i = 0; i < items02.length; i++) {
+  kirinObserver.observe(items02[i]);
+}
