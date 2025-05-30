513bd47c1d971f96b2c5a54384092f631789784c
Rikuto <mikadorikuto@gmail.com>
2025-05-30 16:54:34 +0900

    updated my web_practice

---
+++ education_homework_site/components/script.js
@@ -0,0 +1,48 @@
+const loadingAreaOrange = document.querySelector("#loading");
+const loadingAreaBule = document.querySelector("#loading-screen");
+const loadingText = document.querySelector("#loading p");
+
+window.addEventListener("load", () => {
+    loadingAreaOrange.animate(
+        {
+            opacity: [1, 0],
+            visibility: 'hidden',
+        },
+        {
+            duration: 2000,
+            delay: 1200,
+            easing: 'ease',
+            fill: 'forwards',
+        },
+    );
+
+    loadingAreaBule.animate(
+        {
+            translate: ["0 100vh", "0 0", "0-100vh"],
+        },
+        {
+            duration: 2000,
+            delay: 800,
+            easing: 'ease',
+            fill: 'forwards',
+        },
+    );
+
+    loadingText.animate(
+        [
+            {
+                opacity: 1,
+                offset: .8,
+            },
+            {
+                opacity: 1,
+                offset: 1,
+            },
+        ],
+        {
+            duration: 1200,
+            easing: 'ease',
+            fill: 'forwards',
+        }
+    );
+});
--- my_blog_site/components/script.js
+++ my_blog_site/components/script.js
@@ -65,3 +65,4 @@ function setHeroHeight() {
 window.addEventListener('resize', setHeroHeight);
 window.addEventListener('load', setHeroHeight);
 
+.
