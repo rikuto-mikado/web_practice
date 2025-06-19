5e33c96521d92d31e85ec99da490cb7cd250a68d
Rikuto <mikadorikuto@gmail.com>
2025-06-19 17:01:11 +0900

    updated my web_practice to make css

---
+++ report_practice/components/script.js
@@ -0,0 +1,14 @@
+const targets = document.querySelectorAll(".zoom-on-view");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+            observer.unobserve(entry.target);
+        }
+    });
+}, {
+    threshold: 0.1
+});
+
+targets.forEach(target => observer.observe(target));
