3ab1687517c060eb7c2e7e17c1af175932248fc1
Rikuto <mikadorikuto@gmail.com>
2025-06-23 22:33:45 +0900

    added some ejs and sass files

--- lifestyle_site/app.js
+++ lifestyle_site/app.js
@@ -1,13 +1,46 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
+const sassMiddleware = require('sass-middleware');
+
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', './views');
 
+/* sass_middle_ware */
+app.use (
+  sassMiddleware({
+    src: path.join(__dirname, "sass"),
+    dest: path.join(__dirname, 'public/css'),
+    debug: true,
+    outputStyle: 'compressed',
+    prefix: '/css'
+  })
+);
+
+
+/* img */
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+/* ejs_setting */
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+/* routining */
 app.get('/', (req, res) => {
-    res.render('index', { title: "EJS_page", message: "Hello EJS!!"});
+  res.render('pages/home', { title: 'ホーム' });
 });
 
+app.get('/about', (req, res) => {
+  res.render('pages/about', { title: "About Us" })
+});
+
+app.get('contact', (req, res) => {
+  res.render('pages/contact', { title: ' Contact Us' });
+});
+
+
+/* lanching_server */
 app.listen(3000, () => {
-  console.log('This site is working on http://localhost:3000');
+  console.log('http://localhost:3000');
 });
