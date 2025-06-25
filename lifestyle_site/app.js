48be2030ecb14e26b319b6eb353b3ffc449df248
Rikuto <mikadorikuto@gmail.com>
2025-06-25 15:57:26 +0900

    updated home.ejs a litte, and layout.ejs

--- lifestyle_site/app.js
+++ lifestyle_site/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
 
@@ -9,6 +10,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 /* EJS_template */
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+/* EJS_layout */
+app.use(expressLayouts);
+app.set('layout', 'layout');
 
 /* routing */
 app.get('/', (req, res) => {
