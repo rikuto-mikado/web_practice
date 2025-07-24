8f399064906114b4a4dfd11cdb36d6f2ba0a86e0
Rikuto <mikadorikuto@gmail.com>
2025-07-24 18:36:47 +0900

    add some code

--- news_site/src/routes/news.js
+++ news_site/src/routes/news.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const axios = require('axios');
+
 const newsRouter = express.Router();
 
-newsRouter.get('', async(req, res) => {
-    res.render('partial/news');
+newsRouter.get('/', async (req, res, next) => {
+  try {
+    const { data } = await axios.get('https://raddy.co.uk/wp-json/wp/v2/posts/');
+    res.render("news", { articles: newsAPI.data })
+  } catch (err) {
+    if(err.response) {
+        console.log(err.response.data)
+        console.log(err.response.status)
+        console.log(err.response.headers)
+    } else if(err.requiest) {
+        console.log(err.requiest)
+    } else {
+        console.error('Error', err.message)
+    }
+  }
 });
 
 module.exports = newsRouter;
